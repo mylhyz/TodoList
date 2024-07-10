2bf6ad58b3858ed0f07742e8f5eecf23f8a18ee3
mylhyz <mylhyz@gmail.com>
2024-07-10 14:22:09 +0800

    init spring boot [java+gradle+jdk22]

---
+++ TodoListServer_SpringBoot/src/test/java/io/viper/spring/todolist/TodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package io.viper.spring.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
