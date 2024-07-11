ce9b774f9a0abf8ffa3aa61c116d41251b905fcd
mylhyz <mylhyz@gmail.com>
2024-07-11 17:00:23 +0800

    删除 TodoLIstApp_Kotlin 错误命名

--- TodoLIstApp_Kotlin/app/src/main/java/com/lhyz/android/todolist/BasePresenter.kt
+++
@@ -1,9 +0,0 @@
-package com.lhyz.android.todolist
-
-/**
- * hello,android
- * @author lhyz on 2017/2/6
- */
-interface BasePresenter {
-    fun start()
-}
--- TodoLIstApp_Kotlin/app/src/main/java/com/lhyz/android/todolist/BaseView.kt
+++
@@ -1,9 +0,0 @@
-package com.lhyz.android.todolist
-
-/**
- * hello,android
- * @author lhyz on 2017/2/6
- */
-interface BaseView<T> {
-    fun setPresenter(presenter: T)
-}
